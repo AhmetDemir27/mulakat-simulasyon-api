9b980763ac9778bebef9880e4c012e12c317eded
Ahmet Demir <ad1292365@gmail.com>
2025-10-09 18:10:26 +0300

    Initial commit

---
+++ src/main/java/com/simulasyon/mulakat_simulasyon_api/MulakatSimulasyonApiApplication.java
@@ -0,0 +1,13 @@
+package com.simulasyon.mulakat_simulasyon_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MulakatSimulasyonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MulakatSimulasyonApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simulasyon/mulakat_simulasyon_api/MulakatSimulasyonApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulasyon.mulakat_simulasyon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MulakatSimulasyonApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
